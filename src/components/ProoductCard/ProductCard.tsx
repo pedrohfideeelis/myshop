52acca3e331b99e0fbd19ac0a842bbb8e47adf0f
pedrohfideeelis <phzimnerdola@gmail.com>
2024-07-09 11:12:36 -0300

    update product card

--- src/components/ProoductCard/ProductCard.tsx
+++ src/components/ProoductCard/ProductCard.tsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./styles";
 import { FiShoppingCart } from "react-icons/fi";
+import axios from "axios";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: { rate: number, count: number };
+}
 
 export const ProductCard: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products');
+        const productsData: Product[] = response.data;
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error fetching the products", error);
+      }
+    }
+
+    getProducts();
+  }, []);
+
   return (
-    <S.Card>
-      <S.ProductImg src="" alt="" />
-      <S.ProductTitle></S.ProductTitle>
-      <S.ReviewPriceContainer>
-        <S.Review></S.Review>
-        <S.Price></S.Price>
-      </S.ReviewPriceContainer>
-      <S.AddToCartButton>
-        Adicionar ao Carrinho <FiShoppingCart />
-      </S.AddToCartButton>
-    </S.Card>
+    <>
+      {products.map((product) => (
+        <S.Card key={product.id}>
+          <S.ProductImg src={product.image} alt={product.title} />
+          <S.ProductTitle>{product.title}</S.ProductTitle>
+          <S.ReviewPriceContainer>
+            <S.Review>{product.rating.rate}</S.Review>
+            <S.Price>${product.price}</S.Price>
+          </S.ReviewPriceContainer>
+          <S.ButtonWrapper>
+            <S.AddToCartButton>
+              Adicionar ao Carrinho <FiShoppingCart />
+            </S.AddToCartButton>
+          </S.ButtonWrapper>
+        </S.Card>
+      ))}
+    </>
   );
 };
--- src/components/ProoductCard/styles.ts
+++ src/components/ProoductCard/styles.ts
@@ -1,15 +1,45 @@
 import { styled } from "styled-components";
 
-export const Card = styled.article``;
+export const Card = styled.article`
+  background-color: white;
+  width: 100%;
+  padding: 1rem;
+  margin: 0 auto;
+`;
 
-export const ProductImg = styled.img``;
+export const ProductImg = styled.img`
+  width: 90%;
+`;
 
-export const ProductTitle = styled.h2``;
+export const ProductTitle = styled.h2`
+  font-weight: 500;
+  font-size: 1.2rem;
+`;
 
-export const ReviewPriceContainer = styled.div``;
+export const ReviewPriceContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const Review = styled.span``;
 
-export const Price = styled.span``;
+export const Price = styled.strong``;
 
-export const AddToCartButton = styled.span``;
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const AddToCartButton = styled.button`
+  border: none;
+  border-radius: 5px;
+  height: 30px;
+  padding: 0 1rem;
+  background-color: cornflowerblue;
+  color: white;
+
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+`;
